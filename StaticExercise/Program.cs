574c51fa92db06768de43e2cc2bc5a8e4a25296b
Colin Shanny <cshannyy@gmail.com>
2020-11-06 11:39:59 -0600

    fahrenheight and celsius

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           var Celsius = TempConverter.FahrenheitToCelsius(68);
+           
+            Console.WriteLine($"Celsius = {Celsius} degrees");
+
+            var Fahrenheight = TempConverter.CelsiusToFareinheight(20);
+
+            Console.WriteLine($"Fahrenheight = {Fahrenheight} degrees");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            var result = (Fahrenheit - 32) / 1.8;
+
+                return result;
+        }
+
+        public static double CelsiusToFareinheight(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
